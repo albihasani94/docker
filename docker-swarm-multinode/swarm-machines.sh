aaae129e0f782c29b07f5943c65d9996800a13e7
ahasani <albin.hasani@ikubinfo.al>
2018-06-06 17:44:21 +0200

    Leave hyperv hell for azure heaven

--- docker-swarm-multinode/swarm-machines.sh
+++ docker-swarm-multinode/swarm-machines.sh
@@ -10,7 +10,8 @@ echo "======> Creating $managers manager machines ...";
 for node in $(seq 1 $managers);
 do
 	echo "======> Creating manager$node machine ...";
-	docker-machine create -d hyperv --hyperv-virtual-switch "Default Switch" manager$node;
+	#docker-machine create -d hyperv --hyperv-virtual-switch "Default Switch" manager$node;
+	docker-machine create --driver azure --azure-subscription-id <subscription-id> --azure-size Standard_A1_v2 manager$node;
 done
 
 # create worker machines
@@ -18,7 +19,8 @@ echo "======> Creating $workers worker machines ...";
 for node in $(seq 1 $workers);
 do
 	echo "======> Creating worker$node machine ...";
-	docker-machine create -d hyperv --hyperv-virtual-switch "Default Switch" worker$node;
+	#docker-machine create -d hyperv --hyperv-virtual-switch "Default Switch" worker$node;
+	docker-machine create --driver azure --azure-subscription-id <subscription-id> --azure-size Standard_A1_v2 worker$node;
 done
 
 # list all machines
